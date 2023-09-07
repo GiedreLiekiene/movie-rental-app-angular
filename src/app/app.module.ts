0eef0cc3ab399d1215c56c4ccf3e5084eaf38196
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-07 15:17:51 +0300

    Add movie-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, HeaderComponent, MovieCardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
