d0e9cc1605327440f43cf13952f1483ccf64ab93
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-07 16:42:49 +0300

    Inject the movie service in the component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+  styleUrls: ['./movie-card.component.css'],
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
+  movies: any[] = [];
 
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    //Load movies when the component is initialized
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    //use the movieService to get the movies
+    this.movies = this.movieService.getMovies();
+  }
+
+  rentMovie(movieName: string) {
+    this.movieService.rentMovie(movieName);
+  }
 }
