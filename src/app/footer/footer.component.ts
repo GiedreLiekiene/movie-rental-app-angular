02bb110720000da1346462ca838891767bcb0d08
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-07 14:06:52 +0300

    Add footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import './footer.component.css';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent {}
