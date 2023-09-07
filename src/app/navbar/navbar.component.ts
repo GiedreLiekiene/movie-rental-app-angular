6646cf2270b2f3b57f76b75f1a9b15d57a369ce8
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-07 14:06:16 +0300

    Add navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import './navbar.component.css';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {}
