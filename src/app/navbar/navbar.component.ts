6c4a680d3f0fccc0d33deb869ec5b40624ea00d5
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-08 16:37:44 +0300

    Update navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-import './navbar.component.css';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  movies: any[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    //Load movies when the component is initialized
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    //use the movieService to get the movies
+    this.movies = this.movieService.getMovies();
+  }
+
+  rentMovie(movieName: string) {
+    this.movieService.rentMovie(movieName);
+  }
+}
