2dbebefbb0bf32edc4f2d2e73d155bb15448392b
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-12 11:14:14 +0300

    Add Home component logic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  movies: any[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    //Load movies when the component is initialized
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    //use the movieService to get the movies
+    this.movies = this.movieService.getMovies();
+  }
+
+  rentMovie(movieName: string) {
+    this.movieService.rentMovie(movieName);
+  }
+}
