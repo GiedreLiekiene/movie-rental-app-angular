2582e8d1412e3d4645fbcfde543a4c7f23840750
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-13 17:41:36 +0300

    Add the time window (increment & decrement)

--- src/app/components/rented-movies/rented-movies.component.ts
+++ src/app/components/rented-movies/rented-movies.component.ts
@@ -19,6 +19,22 @@ export class RentedMoviesComponent implements OnInit {
     this.yourMovies = this.YourMoviesService.getYourMovies();
   }
 
+  decrementRentalTime(movie: any) {
+    // Decrement the rental time by 12 hours, but not below 12 hours
+    const rentalTimeInHours = parseInt(movie.time);
+    if (rentalTimeInHours > 12) {
+      movie.time = rentalTimeInHours - 12 + 'h';
+    }
+  }
+
+  incrementRentalTime(movie: any) {
+    // Increment the rental time by 12 hours, but not above 168 hours (1 week)
+    const rentalTimeInHours = parseInt(movie.time);
+    if (rentalTimeInHours < 168) {
+      movie.time = rentalTimeInHours + 12 + 'h';
+    }
+  }
+
   removeMovie(movie: any) {
     const movieIndex = this.yourMovies.findIndex((m) => m.name === movie.name);
     if (movieIndex !== -1) {
