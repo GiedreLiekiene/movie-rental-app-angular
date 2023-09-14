72e343c27fa53d9947de2595cb9c39106a74f217
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-14 14:53:02 +0300

    Update Login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login(): void {
+    const email = this.email.trim();
+    const password = this.password.trim();
+
+    if (email === '' || password === '') {
+      alert('Both email and password fields are required.');
+      return;
+    }
+
+    // Simulation of successful login
+    this.router.navigate(['/home']);
+  }
+}
