34fc5f1ad1713eb118ce157b5b4cff5f5dad2e01
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-14 11:03:15 +0300

    Create Profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  name = 'John';
+  surname = 'Newman';
+  email = 'john.newman@mail.com';
+
+  resetPassword() {
+    // Implement password reset functionality here if needed
+    console.log('Password reset functionality not implemented yet.');
+  }
+
+  resetEmail() {
+    const newEmail = prompt('Enter your new email:');
+
+    if (newEmail !== null && newEmail.trim() !== '') {
+      // Basic email validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (emailRegex.test(newEmail)) {
+        this.email = newEmail;
+      } else {
+        alert('Invalid email format. Please enter a valid email.');
+      }
+    } else if (newEmail !== null) {
+      alert('Email cannot be empty. Please enter a valid email.');
+    }
+  }
+}
