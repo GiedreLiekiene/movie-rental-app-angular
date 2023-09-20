be8bb9124840f064ab22c020bc68a0256521854b
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-20 15:47:50 +0300

    Remove duplicate code

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,22 +6,6 @@ import { MovieService } from '../../services/movie.service';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
-  movies: any[] = [];
-
+export class FooterComponent {
   constructor(private movieService: MovieService) {}
-
-  ngOnInit(): void {
-    //Load movies when the component is initialized
-    this.loadMovies();
-  }
-
-  loadMovies() {
-    //use the movieService to get the movies
-    this.movies = this.movieService.getMovies();
-  }
-
-  rentMovie(movieName: string) {
-    this.movieService.rentMovie(movieName);
-  }
 }
