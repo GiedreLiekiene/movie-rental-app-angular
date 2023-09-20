ad72e343fa92343531b209e86bc122011aa9788e
GiedreLiekiene <giedre.valanciute@gmail.com>
2023-09-20 16:06:24 +0300

    Remove duplicate code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,22 +6,4 @@ import { MovieService } from '../../services/movie.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  movies: any[] = [];
-
-  constructor(private movieService: MovieService) {}
-
-  ngOnInit(): void {
-    //Load movies when the component is initialized
-    this.loadMovies();
-  }
-
-  loadMovies() {
-    //use the movieService to get the movies
-    this.movies = this.movieService.getMovies();
-  }
-
-  rentMovie(movieName: string) {
-    this.movieService.rentMovie(movieName);
-  }
-}
+export class HomeComponent {}
--- src/app/components/your-movies/your-movies.component.ts
+++ src/app/components/your-movies/your-movies.component.ts
@@ -6,33 +6,4 @@ import { YourMoviesService } from 'src/app/services/your-movies.service';
   templateUrl: './your-movies.component.html',
   styleUrls: ['./your-movies.component.css'],
 })
-export class YourMoviesComponent implements OnInit {
-  yourMovies: any[] = []; // Declaring yourMovies as an array property
-
-  constructor(private yourMoviesService: YourMoviesService) {}
-
-  ngOnInit(): void {
-    //loading yourMovies array when the component is initialized
-    this.loadYourMovies();
-  }
-
-  loadYourMovies() {
-    //use the yourMoviesService to get the yourMovies array
-    this.yourMovies = this.yourMoviesService.getYourMovies();
-  }
-
-  removeMovie(movie: any) {
-    const movieIndex = this.yourMovies.findIndex((m) => m.name === movie.name);
-    if (movieIndex !== -1) {
-      this.yourMovies.splice(movieIndex, 1);
-      // Update the table after removing the movie
-      this.loadYourMovies();
-      // Increment the count in stock value in the home page movie list
-      this.updateHomePageMovieList(movie.name);
-    }
-  }
-
-  updateHomePageMovieList(movieName: string) {
-    this.yourMoviesService.updateHomePageMovieList(movieName);
-  }
-}
+export class YourMoviesComponent {}
